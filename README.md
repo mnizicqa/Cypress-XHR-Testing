336e30e45f9b173507c1a14d0b72e0859adc9b78
Mario Nizic <marionizic@msn.com>
2024-02-10 11:28:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cypress Project XHR testing
+
+This is a project which I have done to learn Cypress testing framework. The goal of the project was to test XHRs using [Cypress example site](https://example.cypress.io/commands/network-requests).Tests are written using Cypress version 13.6.3. Main browser used for running tests was Google Chrome version 121. Tests also work on Microsoft Edge and Mozilla Firefox browsers.
+
+You can install all of the dependencies using command in the terminal **npm install**. To run the Cypress, just use the command **npx cypress open** in the terminal.
---
+++ cypress/e2e/network-requests.js
@@ -0,0 +1,71 @@
+/// <reference types = "cypress"/>
+
+describe("Network Requests", () => {
+  let message = "Unable to find comment!";
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/commands/network-requests");
+  });
+
+  it("Get Request", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: "**/comments/*",
+      },
+      {
+        body: {
+          postId: 1,
+          id: 1,
+          name: "Test number 1",
+          email: "tester@test.com",
+          body: "Best ways to test XHR requests",
+        },
+      }
+    ).as("getComment");
+
+    cy.get(".network-btn").click();
+
+    cy.wait("@getComment").its("response.statusCode").should("eq", 200);
+  });
+
+  it("Post Request", () => {
+    cy.intercept("POST", "/comments").as("postComment");
+
+    cy.get(".network-post").click();
+
+    cy.wait("@postComment").should(({ request, response }) => {
+      console.log(request);
+
+      expect(request.body).to.include("email");
+
+      console.log(response);
+
+      expect(response.body).to.have.property("email", "hello@cypress.io");
+
+      expect(request.headers).to.have.property("content-type");
+      expect(request.headers).to.have.property(
+        "origin",
+        "https://example.cypress.io"
+      );
+    });
+  });
+  it("Put Request", () => {
+    cy.intercept(
+      {
+        method: "PUT",
+        url: "**/comments/*",
+      },
+      {
+        statusCode: 404,
+        body: { error: message },
+        delay: 500,
+      }
+    ).as("putComment");
+
+    cy.get(".network-put").click();
+
+    cy.wait("@putComment");
+
+    cy.get(".network-put-comment").should("contain", message);
+  });
+});
